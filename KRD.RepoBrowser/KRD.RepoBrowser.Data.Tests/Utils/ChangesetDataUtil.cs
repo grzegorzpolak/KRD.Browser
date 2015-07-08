d23f4ee97e95e978f31600a79f9076e5e6d4604b
grzegorzpolak <grzegorz.polak@mail.com>
2015-07-08 11:32:18 +0200

    Uaktualnione pakiety, poczatek zmiany autoryzacji

---
+++ KRD.RepoBrowser/KRD.RepoBrowser.Data.Tests/Utils/ChangesetDataUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using KRD.RepoBrowser.Data.Models;
+
+namespace KRD.RepoBrowser.Data.Tests.Utils
+{
+  public class ChangesetDataUtil
+  {
+    public static Changeset Create(
+      string userName, string repositoryName, string branchName, DateTime timestamp)
+    {
+      return new Changeset
+               {
+                 BranchName = branchName,
+                 ChangeId = Guid.NewGuid().ToString(),
+                 RepositoryName = repositoryName,
+                 Username = userName,
+                 Timestamp = timestamp
+               };
+    }
+  }
+}
